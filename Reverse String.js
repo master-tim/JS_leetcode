4413a22be0bee52956ed7fea28a76ab67c157abe
tim <dzhoroev1@gmail.com>
2023-03-21 17:10:29 +0900

    Reverse String

---
+++ Reverse String.js
@@ -0,0 +1,20 @@
+/**
+ * @param {character[]} s
+ * @return {void} Do not return anything, modify s in-place instead.
+ */
+var reverseString = function(s) {
+    var left = 0,  right = s.length-1 ;
+    
+    while (left < right){
+        var temp = s[left];
+        
+        s[left] = s[right];
+        s[right] = temp;
+        
+        left ++;
+        right --;
+    }
+    
+    return s;
+    
+};
