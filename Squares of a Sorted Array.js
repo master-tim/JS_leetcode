032cc82b28941bf5562bfef54397f1fdc6d257b6
tim <dzhoroev1@gmail.com>
2023-03-21 17:20:39 +0900

    Squares of a Sorted Array

---
+++ Squares of a Sorted Array.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var sortedSquares = function(nums) {
+    
+    for (i in nums){
+        nums[i] *= nums[i];
+    }
+    
+    nums.sort((a, b) => a - b);
+    
+    return nums;
+    
