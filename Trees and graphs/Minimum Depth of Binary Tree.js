8c141a9d6553704c93b6cc0d9f93c3e93fa7d8d9
tim <dzhoroev1@gmail.com>
2023-03-26 17:01:00 +0900

    Minimum Depth of Binary Tree

---
+++ Trees and graphs/Minimum Depth of Binary Tree.js
@@ -0,0 +1,26 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var minDepth = function(root) {
+    if(!root)
+           return 0;
+
+   let left = minDepth(root.left);
+   let right = minDepth(root.right);
+
+   if(left==0 || right==0){
+       return (1 + left + right);
+   }
+   else{
+       return 1 + Math.min(left,right);
+   }
+};
